24677d48fa4efa43ce084e9b9517b956df3ed4e2
HashEngineering <hashengineeringsolutions@gmail.com>
2024-03-18 07:27:11 -0700

    chore: initialize lib

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//mod fermented;
+pub mod fetch_identity;
+
+extern crate ferment_macro;
+
+use platform_value::types::binary_data::BinaryData;
+
+
+#[ferment_macro::export]
+pub fn get_binary_data() -> BinaryData {
+    BinaryData::new(vec![])
+}
+#[ferment_macro::export]
+pub fn get_binary_data2() -> BinaryData {
+     BinaryData(vec![0, 1, 2, 3])
+}
