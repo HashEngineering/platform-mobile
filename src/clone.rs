83c1dab96a518f17955cc26f91587d12393c4657
HashEngineering <hashengineeringsolutions@gmail.com>
2024-05-16 07:54:54 -0700

    feat: add DocumentV0 clone

--- src/clone.rs
+++ src/clone.rs
@@ -1,5 +1,32 @@
+use std::collections::BTreeMap;
 use dpp::document::Document;
+use dpp::document::v0::DocumentV0;
+use dpp::identity::identity_public_key::TimestampMillis;
+use dpp::identity::identity_public_key::KeyID;
+use dpp::prelude::{BlockHeight, CoreBlockHeight, Revision};
 use platform_value::{Value, ValueMap};
+
+#[ferment_macro::export]
+pub fn KeyID_clone(id: KeyID) -> KeyID {
+    id.clone()
+}
+
+#[ferment_macro::export]
+pub fn Revision_clone(revision: Revision) -> Revision {
+    revision.clone()
+}
+#[ferment_macro::export]
+pub fn TimestampMillis_clone(time: TimestampMillis) -> TimestampMillis {
+    time.clone()
+}
+#[ferment_macro::export]
+pub fn CoreBlockHeight_clone(height: CoreBlockHeight) -> CoreBlockHeight {
+    height.clone()
+}
+#[ferment_macro::export]
+pub fn BlockHeight_clone(height: BlockHeight) -> BlockHeight {
+    height.clone()
+}
 
 #[ferment_macro::export]
 pub fn Value_clone(value: &Value) -> Value {
@@ -12,6 +39,11 @@ pub fn ValueMap_clone(value_map: &ValueMap) -> ValueMap {
 }
 
 #[ferment_macro::export]
+pub fn std_collections_Map_keys_String_values_platform_value_Value_clone(map: BTreeMap<String, Value>) -> BTreeMap<String, Value> {
+    map.clone()
+}
+
+#[ferment_macro::export]
 pub fn Vec_u8_clone(vec: &Vec<u8>) -> Vec<u8> {
     vec.clone()
 }
@@ -20,13 +52,19 @@ pub fn Vec_u8_clone(vec: &Vec<u8>) -> Vec<u8> {
 pub fn Document_clone(document: &Document) -> Document {
     document.clone()
 }
+
+#[ferment_macro::export]
+pub fn DocumentV0_clone(document: &DocumentV0) -> DocumentV0 {
+    document.clone()
+}
+
 #[ferment_macro::export]
 pub fn Arr_u8_32_clone(slice: [u8; 32]) -> [u8; 32] {
     slice.clone()
 }
 
 #[ferment_macro::export]
-pub fn Arr_u8_20_clone(slice: [u8; 36]) -> [u8; 36] {
+pub fn Arr_u8_20_clone(slice: [u8; 20]) -> [u8; 20] {
     slice.clone()
 }
 
