76104e13ad9da98a3f5e45c7cfbb9d460255a47e
HashEngineering <hashengineeringsolutions@gmail.com>
2024-08-30 19:31:21 -0700

    fix: improve data_contracts module

--- src/data_contracts.rs
+++ src/data_contracts.rs
@@ -1,14 +1,20 @@
 use std::collections::BTreeMap;
 use std::sync::Arc;
 use dash_sdk::platform::{DocumentQuery, Fetch};
+use dash_sdk::{Error, Sdk};
 use dpp::data_contract::accessors::v0::DataContractV0Getters;
 use dpp::data_contract::DataContract;
 use dpp::document::Document;
+use drive_proof_verifier::types::Documents;
 use platform_value::{Identifier, IdentifierBytes32};
 use platform_value::string_encoding::Encoding;
+use rs_dapi_client::RequestSettings;
+use rs_dapi_client::transport::BoxFuture;
 use tokio::runtime::Builder;
-use crate::config::Config;
+use crate::config::{Config, DPNS_DATACONTRACT_ID, EntryPoint};
 use crate::logs::setup_logs;
+use crate::provider::Cache;
+use crate::sdk::{create_dash_sdk, DashSdk};
 
 #[derive(Clone, Debug)]
 #[ferment_macro::export]
@@ -41,61 +47,102 @@ impl Into<DataContractFFI> for DataContract {
     }
 }
 
-#[ferment_macro::export]
-pub fn fetch_data_contract(contract_id: Identifier,
-                                 quorum_public_key_callback: u64,
-                                 data_contract_callback: u64) -> Result<DataContractFFI, String> {
-    setup_logs();
+impl Into<DataContractFFI> for Arc<DataContract> {
+    fn into(self) -> DataContractFFI {
+        (*self).clone().into()
+    }
+}
 
-    //let rt = tokio::runtime::Runtime::new().expect("Failed to create a runtime");
-    let rt = Builder::new_current_thread()
-        .enable_all() // Enables all I/O and time drivers
-        .build()
-        .expect("Failed to create a runtime");
+fn fetch_data_contract_with_retry(
+    sdk: Arc<Sdk>,
+    data_contract_id: Identifier,
+    request_settings: RequestSettings,
+    retries_left: usize,
+) -> BoxFuture<'static, Result<Option<DataContract>, Error>> {
+    Box::pin(async move {
+        match DataContract::fetch_with_settings(&sdk, data_contract_id.clone(), request_settings).await {
+            Ok(Some(data_contract)) => Ok(Some(data_contract)),
+            Ok(None) => {
+                if retries_left > 1 {
+                    if data_contract_id == Identifier::from(dpns_contract::ID_BYTES) ||
+                        data_contract_id == Identifier::from(dashpay_contract::ID_BYTES) {
+                        return fetch_data_contract_with_retry(sdk, data_contract_id, request_settings, retries_left - 1).await;
+                    }
+                }
+                Ok(None)
+            }
+            Err(error) => {
+                if retries_left > 1 {
+                    if error.to_string().contains("contract not found error") {
+                        if data_contract_id == Identifier::from(dpns_contract::ID_BYTES) ||
+                            data_contract_id == Identifier::from(dashpay_contract::ID_BYTES) {
+                            return fetch_data_contract_with_retry(sdk, data_contract_id, request_settings, retries_left - 1).await;
+                        }
+                    }
+                }
+                Err(error)
+            }
+        }
+    })
+}
+
+#[ferment_macro::export]
+pub fn fetch_data_contract(
+    rust_sdk: *mut DashSdk,
+    data_contract_id: Identifier
+) -> Result<Option<DataContractFFI>, String> {
+    let rt = unsafe { (*rust_sdk).get_runtime() };
 
     // Execute the async block using the Tokio runtime
     rt.block_on(async {
-        let cfg = Config::new();
-        let sdk = if quorum_public_key_callback != 0 {
-            cfg.setup_api_with_callbacks(quorum_public_key_callback, data_contract_callback).await
-        } else {
-            cfg.setup_api().await
-        };
+        let sdk = unsafe { (*rust_sdk).get_sdk() };
 
-        let data_contract_id = contract_id;
         tracing::warn!("using existing data contract id and fetching...");
-        let contract =
-            DataContract::fetch(&sdk, data_contract_id.clone())
-                .await;
-        match contract {
-            Ok(Some(data_contract)) => Ok(data_contract.into()),
-            Ok(None) => Err("data contract not found".to_string()),
-            Err(e) => Err(e.to_string())
+        // let contract =
+        //     DataContract::fetch(&sdk, data_contract_id.clone())
+        //         .await;
+        // match contract {
+        //     Ok(Some(data_contract)) => Ok(data_contract.into()),
+        //     Ok(None) => Err("data contract not found".to_string()),
+        //     Err(e) => Err(e.to_string())
+        // }
+
+        match unsafe { (*rust_sdk).get_data_contract(&data_contract_id) } {
+            Some(data_contract) => Ok(Some(data_contract.into())),
+            None => {
+                let request_settings = unsafe { (*rust_sdk).get_request_settings() };
+                match (DataContract::fetch_with_settings(&sdk, data_contract_id.clone(), request_settings)
+                    .await) {
+                    Ok(Some(data_contract)) => {
+                        unsafe { (*rust_sdk).add_data_contract(&data_contract); };
+                        let data_contract_ffi: DataContractFFI = data_contract.into();
+                        Ok(Some(data_contract_ffi))
+                    },
+                    Ok(None) => return Ok(None),//Err("data contract not found".to_string()),
+                    Err(e) => return Err(e.to_string())
+                }
+            }
         }
     })
 }
 
 #[test]
 fn get_data_contract_test() {
-    let config = Config::new();
-
+    let mut sdk = create_dash_sdk(0, 0);
     let data_contract = fetch_data_contract(
-        Identifier(IdentifierBytes32(config.existing_data_contract_id.into())),
-        0,
-        0
+        & mut sdk,
+        Identifier::from(dpns_contract::ID_BYTES)
     ).unwrap();
     tracing::info!("dpns: {:?}", data_contract);
 }
 
 #[test]
 fn get_missing_data_contract_test() {
-    let config = Config::new();
-
+    let mut sdk = create_dash_sdk(0, 0);
     let data_contract_result = fetch_data_contract(
+        & mut sdk,
         Identifier::from_string("Fds5DDfXoLwpUZ71AAVYZP1uod8S7Ze2bR28JExBvZKR", Encoding::Base58).expect("identifier"),
-        0,
-        0
     );
 
-    assert!(data_contract_result.is_err());
+    assert!(data_contract_result.is_ok());
 }
